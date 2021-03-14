a48130b19e957bfb2eca73784f94234a0073d9d7
johan <harryjohn026@gmail.com>
2021-03-14 23:46:13 +0530

    first commit

--- Photoupload/settings.py
+++ Photoupload/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
+    
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '&cwd@_7*0^w78&4v+3#&=m6ge_-0fwb(cnp8^=3cq%d@oo!jj^'
 
